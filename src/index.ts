4c90611153ccf503dd58a82f195e6bcbb7075861
Geunsu Ryu <linusdamyo@gmail.com>
2022-01-23 12:16:59 +0900

    https 를 nginx 에서 처리

--- src/index.ts
+++ src/index.ts
@@ -25,37 +25,37 @@ const server = createServer(app).listen(Number(process.env.PORT) || 18000, () =>
   console.log(`run slack bot PORT: ${Number(process.env.PORT) || 18000}`);
 });
 
-let httpsServer;
+// let httpsServer;
 
 if (process.env.NODE_ENV === 'production') {
-  const pemPath = '/etc/letsencrypt/live/wandookongproject.com';
-  const privateKey = fs.readFileSync(`${pemPath}/privkey.pem`, 'utf8');
-  const certificate = fs.readFileSync(`${pemPath}/cert.pem`, 'utf8');
-  const ca = fs.readFileSync(`${pemPath}/chain.pem`, 'utf8');
+  // const pemPath = '/etc/letsencrypt/live/wandookongproject.com';
+  // const privateKey = fs.readFileSync(`${pemPath}/privkey.pem`, 'utf8');
+  // const certificate = fs.readFileSync(`${pemPath}/cert.pem`, 'utf8');
+  // const ca = fs.readFileSync(`${pemPath}/chain.pem`, 'utf8');
 
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-  };
+  // const credentials = {
+  //   key: privateKey,
+  //   cert: certificate,
+  //   ca: ca
+  // };
 
-  httpsServer = https.createServer(credentials, app).listen(443, () => {
-    console.log('HTTPS server running on PORT 443');
-  });
+  // httpsServer = https.createServer(credentials, app).listen(443, () => {
+  //   console.log('HTTPS server running on PORT 443');
+  // });
 }
 
 process.on('SIGTERM', () => {
   console.log('SIGTERM signal received: closing HTTP server')
-  httpsServer.close(() => {
-    console.log('HTTPS server closed')
-    closeDb()
-    .then(() => {
-      console.log('DB disconnected.')
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  })
+  // httpsServer.close(() => {
+  //   console.log('HTTPS server closed')
+  //   closeDb()
+  //   .then(() => {
+  //     console.log('DB disconnected.')
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   })
+  // })
   server.close(() => {
     console.log('HTTP server closed')
     closeDb()
